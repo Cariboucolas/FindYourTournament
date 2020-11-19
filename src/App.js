ddb1377064cf80c443bab78803cf6f8d28b582c9
lilykr <lisa-lou.k@hotmail.fr>
2020-11-19 16:59:17 +0100

    font work in progress footer

--- src/App.js
+++ src/App.js
@@ -26,8 +26,7 @@ function App() {
           Profile
         </Link>
         <Switch>
-        <Route path="/">
-          </Route>
+  
           <Route path="/tournaments">
             <SearchTournament />
           </Route>
