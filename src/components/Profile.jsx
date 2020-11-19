b6cdc3dee8c5a8e93ffda539af60e6592d0b09fb
lilykr <lisa-lou.k@hotmail.fr>
2020-11-19 15:55:26 +0100

    setting up components profile and ranking

---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Profile() {
+
+  return(
+    <>
+    <h2>My profile</h2>
+    <img />
+    <p></p>
+    <img />
+    <h3>My stats</h3>
+
+    <h3>Tournaments coming up</h3>
+    </>
+
+  )
+}
+
+export default Profile;
---
+++ src/components/RankingList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RankingCard from './RankingCard';
+
+function RankingList(){
+
+  return(
+    <>
+    <h2>Ranking</h2>
+    <RankingCard />
+    </>
+  )
+}
+
+export default RankingList;
