0255f4f1b55aee3c33fc6b5b6ad066260b434bcf
emilieDelsol <emiliedelsol1@gmail.com>
2020-11-20 03:43:47 +0100

    sauvegarde travail en cours linQ

---
+++ BackHackaton/BackHackaton/BDD/ClassesEfCore/TournamentContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackHackaton.BDD.ClassesEfCore
+{
+    
+        public class TournamentContext : DbContext
+        {
+            public virtual DbSet<Tournament> Tournament { get; set; }
+            public virtual DbSet<Knight> Knight { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=tcp:backhackatondb.database.windows.net,1433;Initial Catalog=HackTeam;Persist Security Info=False;User ID=hackteam;Password=$Hack2020;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+    
+}
