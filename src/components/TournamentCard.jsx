46cb10bc8d87c56917bd927206d29cc628c9626a
Rooarii <rooarii.manuel@gmail.com>
2020-11-20 03:08:50 +0100

    tournament page v2

--- src/components/TournamentCard.jsx
+++ src/components/TournamentCard.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-
+import '../styles/TournamentsList.css'
+import '../styles/TournamentCard.css'
 const TournamentCard = ({tournament}) => {
     const {
         tournamentName, 
         description,
         tournamentLocation,
+        images,
     }=tournament;
     return (
-        <>
-            <h2>{tournamentName}</h2>
-            <p>{description}</p>
-            <p>{tournamentLocation}</p>
-        </>
+        <div className="tournament-card">
+            <img className="tournament-img" src={images} alt={tournamentName}/>
+            <div className="tournament-info">
+                <div>
+                    <h2>{tournamentName}</h2>
+                    <p>{description}</p>
+                    <p>{tournamentLocation}</p>
+                </div>
+                <div className="view">View</div>
+            </div>
+        </div>
     );
 }
 
--- src/components/TournamentsList.jsx
+++ src/components/TournamentsList.jsx
@@ -7,62 +7,146 @@ const TournamentsList = () => {
     const tournamentsArr=
     [
         {
-          "tournamentId": 1,
-          "tournamentName": "Tournoi N°1",
-          "tournamentDate": "1392-01-15T00:00:00",
-          "price": 2,
+          "tournamentId": 6,
+          "tournamentName": "Epiphany tournament",
+          "tournamentDate": "1392-01-06T00:00:00",
+          "price": 1,
           "prize": 100,
-          "tournamentLocation": "Chateau de Versailles",
-          "tournamentType": "Au sol",
-          "description": "Viens de friter contre ton rival",
-          "participants": 25,
+          "tournamentLocation": "Palace of the Dukes of Bourgogne",
+          "tournamentType": "Without Mount",
+          "description": "Epiphany: opening of the Combats, we have been waiting for this since last year",
+          "participants": 50,
           "champion": "",
-          "images": "",
-          "videos": ""
-        },
-        {
-          "tournamentId": 4,
-          "tournamentName": "Tournoi N°4",
-          "tournamentDate": "1392-01-15T00:00:00",
-          "price": 100,
-          "prize": 2500,
-          "tournamentLocation": "Le château de Fontainebleau.",
-          "tournamentType": "Au sol",
-          "description": "Affronte les plus grands adversaire du moment !",
-          "participants": 10,
-          "champion": "",
-          "images": "",
+          "images": "http://milhiecreation.com/imagesHackaton/Tournament/Icon_tournament_6.png",
           "videos": ""
         },
         {
           "tournamentId": 3,
-          "tournamentName": "Tournoi N°3",
-          "tournamentDate": "1392-01-22T00:00:00",
-          "price": 0,
-          "prize": 50,
-          "tournamentLocation": "Chateau d'eau",
-          "tournamentType": "Au sol",
-          "description": "Initiation",
+          "tournamentName": "Spring Tournament",
+          "tournamentDate": "1392-03-20T00:00:00",
+          "price": 1,
+          "prize": 100,
+          "tournamentLocation": "Castle of Faverges",
+          "tournamentType": "Without Mount",
+          "description": "No time to see the tulips bloom, the fights have started again",
+          "participants": 25,
+          "champion": "Holdjamy",
+          "images": "http://milhiecreation.com/imagesHackaton/Tournament/Icon_tournament_3.png",
+          "videos": ""
+        },
+        {
+          "tournamentId": 2,
+          "tournamentName": "Easter tournament",
+          "tournamentDate": "1392-04-15T00:00:00",
+          "price": 4,
+          "prize": 500,
+          "tournamentLocation": "Castle of Roquetaillade",
+          "tournamentType": "With mount",
+          "description": "We are not looking for eggs but for bloodthirsty fighters",
+          "participants": 50,
+          "champion": "Anosdagan",
+          "images": "http://milhiecreation.com/imagesHackaton/Tournament/Icon_tournament_2.png",
+          "videos": ""
+        },
+        {
+          "tournamentId": 5,
+          "tournamentName": "Ascension Tournament",
+          "tournamentDate": "1392-05-13T00:00:00",
+          "price": 10,
+          "prize": 1000,
+          "tournamentLocation": "Castle of Rouen",
+          "tournamentType": "With Mount",
+          "description": "As the symbol of faith expresses it: “He ascended into Heaven; he is seated at the right hand of the Father. He will come again in glory to judge the living and the dead, and his reign will have no end ",
+          "participants": 75,
+          "champion": "Asnosdagan",
+          "images": "http://milhiecreation.com/imagesHackaton/Tournament/Icon_tournament_5.png",
+          "videos": ""
+        },
+        {
+          "tournamentId": 7,
+          "tournamentName": "Pentecote tournament",
+          "tournamentDate": "1392-05-23T00:00:00",
+          "price": 50,
+          "prize": 1000,
+          "tournamentLocation": "Castle of Courtanvaux",
+          "tournamentType": "With Mount",
+          "description": "",
+          "participants": 50,
+          "champion": "",
+          "images": "http://milhiecreation.com/imagesHackaton/Tournament/Icon_tournament_7.png",
+          "videos": ""
+        },
+        {
+          "tournamentId": 4,
+          "tournamentName": "Mother's Day tournament",
+          "tournamentDate": "1392-05-30T00:00:00",
+          "price": 10,
+          "prize": 250,
+          "tournamentLocation": "Castle of Veauce",
+          "tournamentType": "Without Mount",
+          "description": "Many mothers lose their children during this tournament",
+          "participants": 30,
+          "champion": "Creddred",
+          "images": "",
+          "videos": ""
+        },
+        {
+          "tournamentId": 8,
+          "tournamentName": "Assumption tournament",
+          "tournamentDate": "1392-08-15T00:00:00",
+          "price": 50,
+          "prize": 1000,
+          "tournamentLocation": "Castle of Thorens",
+          "tournamentType": "With Mount",
+          "description": "The Assumption of the Blessed Virgin and also of our fighters",
+          "participants": 100,
+          "champion": "",
+          "images": "http://milhiecreation.com/imagesHackaton/Tournament/Icon_tournament_8.png",
+          "videos": ""
+        },
+        {
+          "tournamentId": 9,
+          "tournamentName": "Toussaint tournament",
+          "tournamentDate": "1392-11-01T00:00:00",
+          "price": 25,
+          "prize": 500,
+          "tournamentLocation": "Castle of Falaise",
+          "tournamentType": "Without Mount",
+          "description": "The day after the feast of the dead: enough to honor all these fighters who left too early",
           "participants": 100,
           "champion": "",
           "images": "",
           "videos": ""
         },
         {
-          "tournamentId": 2,
-          "tournamentName": "Tournoi N°2",
-          "tournamentDate": "1392-03-11T00:00:00",
-          "price": 5,
-          "prize": 200,
-          "tournamentLocation": "Chateau de Colas",
-          "tournamentType": "Cheval",
-          "description": "Viens labourer la pelouse",
-          "participants": 10,
+          "tournamentId": 10,
+          "tournamentName": "Charles V tournament ",
+          "tournamentDate": "1392-11-01T00:00:00",
+          "price": 25,
+          "prize": 500,
+          "tournamentLocation": "Castle of Falaise",
+          "tournamentType": "Without Mount",
+          "description": "The day after the feast of the dead: enough to honor all these fighters who left too early",
+          "participants": 100,
           "champion": "",
-          "images": "",
+          "images": "http://milhiecreation.com/imagesHackaton/Tournament/Icon_tournament_9.png",
+          "videos": ""
+        },
+        {
+          "tournamentId": 1,
+          "tournamentName": "Tournament Of the year : Christmas tournament ",
+          "tournamentDate": "1392-12-25T00:00:00",
+          "price": 100,
+          "prize": 12500,
+          "tournamentLocation": "Castle of Vincennes",
+          "tournamentType": "With Mount",
+          "description": "This is the last tournament of the year, you will have the chance to fight in front of the king in person",
+          "participants": 100,
+          "champion": "Creddred",
+          "images": "http://milhiecreation.com/imagesHackaton/Tournament/Icon_tournament_10.png",
           "videos": ""
         }
-    ]
+      ]
 
     // tournament state
     const  [tournaments, setTournaments]=useState([])
@@ -86,10 +170,13 @@ const TournamentsList = () => {
 
     return (
         <>
-            <div>test</div>
-            {tournaments.map(tournament => 
-                <TournamentCard tournament={tournament} />
-            )}
+            <div>Tournaments</div>
+            <div className="container">
+                {tournaments.map(tournament => 
+                    <TournamentCard tournament={tournament} />
+                )}
+            </div>
+            
         </>
     );
 }
