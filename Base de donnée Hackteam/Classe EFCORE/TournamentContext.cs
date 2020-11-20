387f757835e5c1f2146f849f19fd499ac69c3ddf
emilieDelsol <emiliedelsol1@gmail.com>
2020-11-20 03:31:18 +0100

    delete DAL add BDD creation in project and change class Dal by class DataSelect

--- Base de donnée Hackteam/Classe EFCORE/TournamentContext.cs
+++ Base de donnée Hackteam/Classe EFCORE/TournamentContext.cs
@@ -9,8 +9,7 @@ namespace Base_de_donnée_Hackteam
         public virtual DbSet<Knight> Knight { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
-                @"Server=tcp:backhackatondb.database.windows.net,1433;Initial Catalog=HackTeam;Persist Security Info=False;User ID=hackteam;Password=$Hack2020;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            optionsBuilder.UseSqlServer(@"Server=tcp:backhackatondb.database.windows.net,1433;Initial Catalog=HackTeam;Persist Security Info=False;User ID=hackteam;Password=$Hack2020;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
     }
 }
